1cb0812e8b66c52a2493c901df97d942fc6bce49
opilane <opilane@HP-CZC2349HTB>
2023-11-16 11:48:05 +0200

    Add project files.

---
+++ Vitalii_Sokhan_TARgv23.py
@@ -0,0 +1,10 @@
+﻿from sre_constants import IN
+
+
+print("Tere tulemast!")
+kool=input("Mis koolis sa õpid: ") #str kool
+kursus=int(input("Mis kursusel: ")) #int kursus
+print("Tere tulemast kooli "+kool+"!\n0le hea "+str(kursus)+".kursuse õpilaseks!")
+print("Tere tulemast kooli",kool,"!\n0le hea",kursus,".kuursuse õpilaseks!")
+print("Tere tulemast kooli {0}!\n0le hea {1}.kuursuse õpetajaks!".format(kool,kursus))
+
