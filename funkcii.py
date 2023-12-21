1ee90c4871c7a606c29304d10ba17d0336476557
svjfox <151017791+svjfox@users.noreply.github.com>
2023-12-21 13:58:18 +0200

    111

--- funkcii.py
+++ funkcii.py
@@ -1,2 +1,12 @@
 # Privet
+def summa3(arv1: float,arv2: float,arv3: float)->float:
+    '''»щем сумму трех чисел
+    
+    :param float arv1: первое число
+    :param float arv2: второе число
+    :param float arv3: третье число
+    :rtype: float
+    '''
+    summa = arv1 + arv2 + arv3
+    return summa
 
