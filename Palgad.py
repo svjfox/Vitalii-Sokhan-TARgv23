a509e7f68cc3ccd4d39a7624e65230a37472d089
opilane <opilane@HP-CZC2349HTB>
2024-01-11 14:35:17 +0200

    111

---
+++ Palgad.py
@@ -0,0 +1,17 @@
+#from Funktcion import *
+
+
+
+
+#palgad=[1200,2500,750,395,1200]
+#inimesed=["A","B","C","D","E"]
+
+#while True:
+#    print("Lisamine-1")
+#    v=int(input())
+#    if v==1:
+#        k=int(input("Mitu inimest lisame? "))
+#        inimesed,palgad=Lisamine(inimesed, palgad,k)
+#        for i in range(len(palgad)):
+#            print(inimesed[i], "saab katte",palgad[i])
+
