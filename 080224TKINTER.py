3bd702c97ad9b25156f8c59f55be1a643939e522
svjfox <151017791+svjfox@users.noreply.github.com>
2024-02-08 10:00:16 +0200

    08

---
+++ 080224TKINTER.py
@@ -0,0 +1,16 @@
+﻿from tkinter import *
+
+
+
+aken=Tk()
+aken.geometry("600x500")
+aken.title("Pealkiri")
+
+aken.iconbitmap("potential.ico")
+f=Frame(aken,bg='blue',border=10,height=100,width=600)
+l=Label(f,text="Tere",bg='gold',fg='#aa4a44',font='Arial 24',height=3,width=17)
+
+
+f.grid(row=0,column=0) #pack(), place()
+l.pack()
+aken.mainloop()
