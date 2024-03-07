fab76f0fc2a025aa06f90ca49eec6660539de94e
svjfox <svjfox@MSI>
2024-03-07 12:02:05 +0200

    070824

--- 290224Zadanie.py
+++ 290224Zadanie.py
@@ -26,6 +26,7 @@
 
 # 1. Создайте базу данных с тремя связанными таблицами: «Продукты», «Категории» и «Бренды».
 
+from ast import While
 from multiprocessing import connection
 import select
 from sqlite3 import *
@@ -79,6 +80,7 @@ def execute_delete_query(connection, query:str):
         print(f"Ошибка '{e}'")
         
 #-------------SQL запросы----------------
+        
 create_users_table_brand="""
 CREATE TABLE IF NOT EXISTS brand(
 id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -149,6 +151,8 @@ VALUES
    
 """
     
+
+# 4. Напишите несколько (минимум 3) запросов о товарах, включая информацию о категории и бренде.
 select_product = """
     SELECT
     product_name,
@@ -183,16 +187,106 @@ select_product_brand = """
         brand_name = 'brand1';
         
 """
+
+
+# 5. Реализовать возможность добавления в базу новых товаров, категорий и брендов.
+insert_brand = """
+INSERT INTO
+    brand (brand_id, brand_name)
+    VALUES
+    (4, 'brand4');
+    """
+    
+insert_category = """
+INSERT INTO
+    category (category_id, category_name)
+    VALUES
+    (4, 'category4');
+    """
+
+insert_product = """
+INSERT INTO
+    product (product_id, product_name, price, category_id, brand_id)
+    VALUES
+    (16, 'product16', 1600, 4, 4);
+    """
+    
+# 6. Сделать возможным изменение информации о товарах, категориях и брендах. Придумайте свои критерии.
+update_brand = """
+UPDATE
+    brand
+    SET
+    brand_name = 'brand5'
+    WHERE
+    brand_id = 4;
+    """
+    
+update_category = """
+    UPDATE
+    category
+    SET
+    category_name = 'category5'
+    WHERE
+    category_id = 4;
+    """
+
+update_product = """
+    UPDATE
+    product
+    SET
+    product_name = 'product5'
+    WHERE
+    product_id = 16;
+    """
+    
+# 7. Применяйте запросы для удаления продуктов по определенным критериям (например, все продукты определенной категории или бренда).
+delete_brand = """
+    DELETE FROM
+    brand
+    WHERE
+    brand_id = 4;
+    """
+    
+delete_category = """
+    DELETE FROM
+    category
+    WHERE
+    category_id = 4;
+    """
+    
+delete_product = """
+    DELETE FROM
+    product
+    WHERE
+    product_id = 16;
+    """
+    
+# 8. Полностью удалите любую таблицу, а затем восстановите ее заново.
+delete_brand_table = """
+    DROP TABLE brand;
+    """
+    
+delete_category_table = """
+    DROP TABLE category;
+    """
+
+delete_product_table = """
+    DROP TABLE product;
+    """
+    
+# востановление таблиц
+
+
+
+
+
+# показать таблицы!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         
 select_br='SELECT * FROM brand'
 select_cat='SELECT * FROM category'
 select_prod='SELECT * FROM product'
    
     
-#3. Заполните таблицы данными о разных категориях товаров, брендах и товарах.
-
-
-
 
 
 conn=create_connection('C:/Users/svjfo/source/repos/svjfox/Vitalii-Sokhan-TARgv23/AppData/290224Zadanie.db')
@@ -225,6 +319,7 @@ conn=create_connection('C:/Users/svjfo/source/repos/svjfox/Vitalii-Sokhan-TARgv2
 
 
 
+
 # запрос на отображение продуктов, включая информацию о категории и бренде
 
 brand=execute_read_query(conn,select_br)
@@ -240,6 +335,110 @@ print('Продукты:')
 for prod in products:
     print(prod)
 
+    
+# создаем диалоговое окно работы с базой данных
+
+for i in range(1,11):
+    Vybor_deistvija=input("Выберите действие: 1-добавить, 2-удалить, 3-изменить, 4-выбрать, 5-закрыть: ")
+    if Vybor_deistvija=="1":
+        print("Вы выбрали добавление")
+        Vybor_tablicy=input("Выберите таблицу: 1-бренды, 2-категории, 3-продукты: ")
+        if Vybor_tablicy=="1":
+            print("Вы выбрали добавление бренда")
+            execute_query(conn,insert_brand)
+            brand=execute_read_query(conn,select_br)
+            print('Бренды:')
+            for br in brand:
+                print(br)
+        elif Vybor_tablicy=="2":
+            print("Вы выбрали добавление категории")
+            execute_query(conn,insert_category)
+            category=execute_read_query(conn,select_cat)
+            print('Категории:')
+            for cat in category:
+                print(cat)
+        elif Vybor_tablicy=="3":
+            print("Вы выбрали добавление продукта")
+            execute_query(conn,insert_product)
+            products=execute_read_query(conn,select_prod)
+            print('Продукты:')
+            for prod in products:
+                print(prod)
+    elif Vybor_deistvija=="2":
+        print("Вы выбрали удаление")
+        Vybor_tablicy=input("Выберите таблицу: 1-бренды, 2-категории, 3-продукты: ")
+        if Vybor_tablicy=="1":
+            print("Вы выбрали удаление бренда")
+            execute_query(conn,delete_brand)
+            brand=execute_read_query(conn,select_br)
+            print('Бренды:')
+            for br in brand:
+                print(br)
+        elif Vybor_tablicy=="2":
+            print("Вы выбрали удаление категории")
+            execute_query(conn,delete_category)
+            category=execute_read_query(conn,select_cat)
+            print('Категории:')
+            for cat in category:
+                print(cat)
+        elif Vybor_tablicy=="3":
+            print("Вы выбрали удаление продукта")
+            execute_query(conn,delete_product)
+            products=execute_read_query(conn,select_prod)
+            print('Продукты:')
+            for prod in products:
+                print(prod)
+    elif Vybor_deistvija=="3":
+        print("Вы выбрали изменение")
+        Vybor_tablicy=input("Выберите таблицу: 1-бренды, 2-категории, 3-продукты: ")
+        if Vybor_tablicy=="1":
+            print("Вы выбрали изменение бренда")
+            execute_query(conn,update_brand)
+            brand=execute_read_query(conn,select_br)
+            print('Бренды:')
+            for br in brand:
+                print(br)
+        elif Vybor_tablicy=="2":
+            print("Вы выбрали изменение категории")
+            execute_query(conn,update_category)
+            category=execute_read_query(conn,select_cat)
+            print('Категории:')
+            for cat in category:
+                print(cat)
+        elif Vybor_tablicy=="3":
+            print("Вы выбрали изменение продукта")
+            execute_query(conn,update_product)
+            products=execute_read_query(conn,select_prod)
+            print('Продукты:')
+            for prod in products:
+                print(prod)
+    elif Vybor_deistvija=="4":
+        print("Вы выбрали выбор")
+        Vybor_tablicy=input("Выберите таблицу: 1-бренды, 2-категории, 3-продукты: ")
+        if Vybor_tablicy=="1":
+            print("Вы выбрали бренды")
+            brand=execute_read_query(conn,select_br)
+            print('Бренды:')
+            for br in brand:
+                print(br)
+        elif Vybor_tablicy=="2":
+            print("Вы выбрали категории")
+            category=execute_read_query(conn,select_cat)
+            print('Категории:')
+            for cat in category:
+                print(cat)
+        elif Vybor_tablicy=="3":
+            print("Вы выбрали продукты")
+            products=execute_read_query(conn,select_prod)
+            print('Продукты:')
+            for prod in products:
+                print(prod)
+    elif Vybor_deistvija=="5":
+        print("Вы выбрали закрыть")
+        break
+else:
+    print("Неверный ввод")
+    
     
 
 
